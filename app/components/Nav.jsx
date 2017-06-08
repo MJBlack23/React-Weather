979b40651212038e8614732937a60e15ee394a6f
Matt Black <mjblack23@Gmail.com>
2017-06-07 19:52:36 -0500

    'auto-commit'

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,42 @@ const React = require('react');
 const { Link, IndexLink } = require('react-router');
 
 const Nav = React.createClass({
+
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('not yet implemented');
+  },
+
   render: function () {
     return (
-      <nav className='navbar navbar-default'>
-        <h2>React Weather</h2>
-        <IndexLink to='/' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
-        <Link to='/about' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>About</Link>
-        <Link to='/examples' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
-      </nav>
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>React Weather</li>
+            <li>
+              <IndexLink to='/' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to='/about' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>About</Link>
+            </li>
+            <li>
+              <Link to='/examples' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <form onSubmit={ this.onSearch }>
+            <ul className='menu'>
+              <li>
+                <input type='search' placeholder='Search Weather' />
+              </li>
+              <li>
+                <input type='submit' className='button' value='Get Weather' />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
     )
   }
 });
