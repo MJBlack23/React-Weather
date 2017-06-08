8b4a68f4514102192a6c332ff294095c581ed290
Matt Black <mjblack23@Gmail.com>
2017-06-07 19:12:19 -0500

    fixing redirects

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  if (Object.keys(req.headers).indexOf('x-forward-proto') > 0 && req.headers['x-forwarded-proto'] ==='http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
+  if (Object.keys(req.headers).indexOf('x-forward-proto') > 0) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    }
   }
+   next();
 });
 
 app.use(express.static('public'));
