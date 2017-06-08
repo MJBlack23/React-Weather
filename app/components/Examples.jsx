964391a9cd977deb6ebc4e484875ab1a1a35ab3e
Matt Black <mjblack23@Gmail.com>
2017-06-07 20:18:35 -0500

    'auto-commit'

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,23 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const Examples = React.createClass({
   render: function () {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples</p>
+        <h1 className='text-center'>Examples</h1>
+        <p>Here are a few example locations to try out...</p>
+        <ol>
+          <li>
+            <Link to='/?location=springdale'>Springdale, AR</Link>
+          </li>
+          <li>
+            <Link to='/?location=los%20angeles'>Los Angeles, CA</Link>
+          </li>
+          <li>
+            <Link to='/?location=new&20york'>New York, NY</Link>
+          </li>
+        </ol>
       </div>
     )
   }
