1d5beb6f37f632b3449145bc3e4683bb7b4df5bb
Matt Black <mjblack23@Gmail.com>
2017-06-07 21:23:59 -0500

    'auto-commit'

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ const About = React.createClass({
   render: function () {
     return (
       <div>
-        <h1 className='text-center'>About</h1>
+        <h1 className='text-center page-title'>About</h1>
         <p>
           This is a weather application built on React. I have built for the Complete React Web App
           Developer Course.
