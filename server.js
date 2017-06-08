2da72f111e306bbecbf9a165059ad02c55f7c2fe
Matt Black <mjblack23@Gmail.com>
2017-06-07 19:41:52 -0500

    'auto-commit'

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  // if (Object.keys(req.headers).indexOf('x-forward-proto') > 0) {
+  if (Object.keys(process.env).indexOf('NODE_ENV') >= 0) {
     if (req.headers['x-forwarded-proto'] === 'http') {
       next();
     } else {
       res.redirect('http://' + req.hostname + req.url);
     }
-  // } else {
-  //   next();
-  // }
+  } else {
+    next();
+  }
 });
 
 app.use(express.static('public'));
