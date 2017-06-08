33712a58196925b3674fc9bff4ebb10a2d57bb12
Matt Black <mjblack23@Gmail.com>
2017-06-07 19:02:07 -0500

    devops'

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] ==='http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(port, () => {
