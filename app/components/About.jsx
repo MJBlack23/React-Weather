89b2a82030a5bbb51fa1b94022182da613e296ac
Matt Black <mjblack23@Gmail.com>
2017-06-07 20:34:15 -0500

    'auto-commit'

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,25 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const About = React.createClass({
   render: function () {
     return (
       <div>
-        <h3>About</h3>
-        <p>Welcome to the about page.</p>
+        <h1 className='text-center'>About</h1>
+        <p>
+          This is a weather application built on React. I have built for the Complete React Web App
+          Developer Course.
+        </p>
+        <p>
+          This app uses...
+          <ul>
+            <li><a href='http://http://openweathermap.org/' target='_blank'>Open Weather Map</a> to search weather.</li>
+            <li><a href='https://facebook.github.io/react/' target='_blank'>React</a> as a Javascript Framework.</li>
+            <li><a href='http://foundation.zurb.com' target='_blank'>Foundation</a> for display and formatting.</li>
+            <li><a href='https://www.github.com' target='_blank'>Github</a> for version control.</li>
+            <li><a href='https://www.heroku.com/' target='_blank'>Heroku</a> for dev ops.</li>
+          </ul>
+        </p>
       </div>
     )
   }
