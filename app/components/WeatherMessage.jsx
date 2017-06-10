e2ba81043671bdf49d21dc78b7b104d607006079
Matt Black <mjblack23@Gmail.com>
2017-06-09 22:21:39 -0500

    'auto-commit'

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -5,10 +5,15 @@ const WeatherMessage = React.createClass({
     let { temp, location } = this.props;
     return (
       <div>
-        <h3 className='text-center'>It is { temp } in { location }.</h3>
+        <h3 className='text-center'>It is { Math.round(temp) }&#8457; in { toTitleCase(location) }.</h3>
       </div>
     )
   }
 });
 
 module.exports = WeatherMessage;
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
